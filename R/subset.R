e2109a48fa88b2c5a1d34a02b3a595c1ae052307
joethorley <joe@poissonconsulting.ca>
2019-06-30 16:30:53 -0700

    more subset tests

--- R/subset.R
+++ R/subset.R
@@ -1,6 +1,6 @@
 #' @export
 subset.term <- function(x, pars = NULL, ...) {
-  checkor(check_null(pars), check_vector(pars, pars(x), unique = TRUE))
+  checkor(check_null(pars), check_vector(pars, pars(x), unique = TRUE, only = TRUE))
   check_unused(...)
 
   if(!is.null(pars)) {
--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -1,6 +1,9 @@
 context("subset")
 
 test_that("subset.term",{
+  term <- as.term(c("alpha[1]", "alpha[2]", "sigma"))
+  expect_error(subset(term, "beta"), 
+                   "pars must be NULL OR pars can only include values 'alpha' or 'sigma'")
   term <- as.term(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]", 
                     "beta[1,2]", "beta[2,2]", "sigma"))
   expect_identical(subset(term), term)
