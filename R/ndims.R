9b411a9404a7f6e5e8df826343368b44e268f3af
joethorley <joe@poissonconsulting.ca>
2019-06-30 09:31:09 -0700

    ndims

---
+++ R/ndims.R
@@ -0,0 +1,22 @@
+#' Number of Dimensions of an Object
+#'
+#' Retrieves the number of dimensions of an object as defined by dims.
+#'
+#' @param x An R object.
+#' @param ... Unused
+#' @return A vector or list of the number of dimensions.
+#' @export
+ndims <- function(x, ...) {
+  UseMethod("ndims")
+}
+
+#' @export
+ndims.default <- function(x, ...) {
+  length(dims(x))
+}
+
+#' @export
+ndims.matrix <- function(x, ...) 2L
+
+#' @export
+ndims.data.frame <- function(x, ...) 2L
