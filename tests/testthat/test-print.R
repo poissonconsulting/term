8a7d94d590fa136952496d78aaa6141e9cf45333
joethorley <joe@poissonconsulting.ca>
2019-06-30 09:26:38 -0700

    test print

---
+++ tests/testthat/test-print.R
@@ -0,0 +1,13 @@
+context("print")
+
+test_that("print", {
+  expect_identical(capture.output(print(as.term(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]", 
+                  "beta[1,2]", "beta[2,2]", "sigma")))),
+                  c("[1] \"alpha[1]\"  \"alpha[2]\"  \"beta[1,1]\" \"beta[2,1]\" \"beta[1,2]\" \"beta[2,2]\"", 
+"[7] \"sigma\"    "))
+
+  expect_identical(capture.output(print(as.term(c("alpha[1]", "sigma", "alpha[2]", "beta[1,1]", "beta[2,1]", 
+                  "beta[1,2]", "beta[2,2]")))),
+                  c("[1] \"alpha[1]\"  \"sigma\"     \"alpha[2]\"  \"beta[1,1]\" \"beta[2,1]\" \"beta[1,2]\"", 
+"[7] \"beta[2,2]\""))
+})
