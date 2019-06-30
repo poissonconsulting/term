66ae77a840003253f35aab98a9727cfba89f59da
joethorley <joe@poissonconsulting.ca>
2019-06-30 16:20:34 -0700

    subset.term

---
+++ R/subset.R
@@ -0,0 +1,13 @@
+#' @export
+subset.term <- function(x, pars = NULL, ...) {
+  checkor(check_null(pars), check_vector(pars, pars(x), unique = TRUE))
+  check_unused(...)
+
+  if(!is.null(pars)) {
+    pars <- paste0("(^", pars, ("($|\\[))"))
+    pars <- paste0(pars, collapse = ")|(")
+    pars <- paste0("(", pars, ")", collapse = "")
+    x <- x[grepl(pars, x)]
+  }
+  x
+}
---
+++ tests/testthat/test-subset.R
@@ -0,0 +1,13 @@
+context("subset")
+
+test_that("subset.term",{
+  term <- as.term(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]", 
+                    "beta[1,2]", "beta[2,2]", "sigma"))
+  expect_identical(subset(term), term)
+  expect_identical(subset(term, "sigma"), as.term("sigma"))
+  expect_identical(subset(term, c("sigma", "beta")), 
+                   as.term(c("beta[1,1]", "beta[2,1]", 
+                             "beta[1,2]", "beta[2,2]", "sigma")))
+  expect_error(subset(term, "tt"), 
+               "pars must be NULL OR pars can only include values 'alpha', 'beta' or 'sigma'")
+})
