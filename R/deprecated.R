7a5802d2a34ec39be573822cbf919b2339d9bf50
joethorley <joe@poissonconsulting.ca>
2019-07-04 06:59:34 -0700

    no longer deprecate parameters

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,26 +4,3 @@ tdims <- function(x) {
   .Deprecated("tindex")
   tindex(x)
 }
-
-#' @rdname pars
-#' @export
-parameters <- function(x, ...) {
-  .Deprecated("pars")
-  UseMethod("pars")
-}
-
-#' @rdname pars
-#' @export
-`parameters<-` <- function(x, value) {
-  .Deprecated("pars")
-  UseMethod("pars<-", x)
-}
-
-#' @rdname pars
-#' @param parameters A character vector of the new parameter names.
-#' @export
-set_parameters <- function(x, parameters) {
-  .Deprecated("set_pars")
-  pars(x) <- parameters
-  x
-}
