677d91f4cdc3de6d3fd58ce8879fadff3849f6bc
joethorley <joe@poissonconsulting.ca>
2019-07-27 16:05:34 -0700

    subset has na.rm arg

--- R/subset.R
+++ R/subset.R
@@ -2,11 +2,14 @@
 #' 
 #' Subsets a term vector.
 #' 
+#' The term vector is repaired prior to subsetting.
+#' 
 #' @param x A term vector to be subsetted.
 #' @param select A character vector of the names of the parameters to include in the subsetted object.
+#' @param na.rm A flag specifying whether to remove missing or invalid terms.
 #' @param ... Unused.
 #' @return The modified term vector.
-#' @seealso \code{\link{term-vector}}
+#' @seealso \code{\link{term-vector}} and \code{\link{repair_terms}()}
 #' @export
 #' 
 #' @examples 
@@ -14,13 +17,18 @@
 #' "beta[1,2]", "beta[2,2]", "sigma"))
 #' subset(term, "beta")
 #' subset(term, c("alpha", "sigma"))
-subset.term <- function(x, select = NULL, ...) {
-  if(is.null(select)) return(x)
-  check_vector(select, pars(x), unique = TRUE, only = TRUE)
+subset.term <- function(x, select = NULL, na.rm = FALSE, ...) {
+  check_flag(na.rm)
   check_unused(...)
+  
+  if(isTRUE(na.rm)) x <- x[!is.na(x)]
+  if(is.null(select)) return(x)
+  x <- repair_terms(x)
+  if(isTRUE(na.rm)) x <- x[!is.na(x)]
+  
+  check_vector(select, .pars(x), unique = TRUE, only = TRUE)
   
   if(!length(select)) return(term(0L))
   
-  x <- x[pars(x, terms = TRUE) %in% select]
-  x[order(as.integer(factor(pars(x, terms = TRUE), levels = select)))]
+  x[is.na(x) | .pars(x, terms = TRUE) %in% select]
 }
--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -13,8 +13,21 @@ test_that("subset.term",{
                    as.term(c("beta[1,1]", "beta[2,1]", 
                              "beta[1,2]", "beta[2,2]", "sigma")))
   expect_identical(subset(term, c("sigma", "beta")), 
-                   as.term(c("sigma", "beta[1,1]", "beta[2,1]", 
-                             "beta[1,2]", "beta[2,2]")))
+                   as.term(c("beta[1,1]", "beta[2,1]", 
+                             "beta[1,2]", "beta[2,2]", "sigma")))
   expect_error(subset(term, "tt"), 
                "select can only include values 'alpha', 'beta' or 'sigma'")
+})
+
+test_that("subset.term missing values",{
+  expect_identical(subset(NA_term_), NA_term_)
+  expect_identical(subset(NA_term_, na.rm = TRUE), term(0L))
+  expect_identical(subset(c(NA_term_, "a")), c(NA_term_, "a"))
+  expect_identical(subset(c(NA_term_, "a"), na.rm = TRUE), as.term("a"))
+  expect_error(subset(c(NA_term_, "a"), select = "b"), 
+               "select can only include values 'a'")
+  expect_identical(subset(c(NA_term_, "a", "b"), select = "b"), 
+               as.term(c(NA_term_, "b")))
+  expect_identical(subset(c(NA_term_, "a", "b"), na.rm = TRUE, select = "b"), 
+               as.term("b"))
 })
