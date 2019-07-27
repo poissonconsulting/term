d3e6337e10b93e3320daddb254cbbd4c72b1a5fa
joethorley <joe@poissonconsulting.ca>
2019-07-26 19:12:18 -0700

    deprecated message pars

--- R/deprecated.R
+++ R/deprecated.R
@@ -9,14 +9,23 @@ tdims <- function(x) {
 #' @rdname pars
 #' @details \code{parameters()} is deprecated.
 #' @export
-parameters <- function(x, ...) UseMethod("pars")
+parameters <- function(x, ...) {
+  .Deprecated("pars")
+  UseMethod("pars")
+}
 
 #' @rdname pars
 #' @details \code{parameters<-} is deprecated.
 #' @export
-`pararameters<-` <- function(x, value) UseMethod("pars<-", x)
+`pararameters<-` <- function(x, value) {
+  .Deprecated("pars<-")
+  UseMethod("pars<-", x)
+}
 
 #' @rdname pars
 #' @details \code{set_parameters()} is deprecated.
 #' @export
-set_parameters <- function(x, pars) set_pars(x, pars)
+set_parameters <- function(x, pars) {
+  .Deprecated("set_pars")
+  set_pars(x, pars)
+}
