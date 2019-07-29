cb2543e2fa676175ff521de3066772a26ee95a3c
joethorley <joe@poissonconsulting.ca>
2019-07-29 16:13:46 -0700

    npars has repair badge

--- R/npars.R
+++ R/npars.R
@@ -2,17 +2,18 @@
 #'
 #' Gets the number of parameters of an object as returned by \code{\link{pars}()}.
 #' 
-#' By default (\code{na.rm = TRUE}) the function returns \code{NA_integer_}
-#' if the term vector includes invalid or missing term elements.
+#' By default (\code{na.rm = TRUE}) the function repairs the vector and 
+#' returns \code{NA_integer_} if the term vector includes missing term elements.
 #'
 #' @param x An R object.
 #' @param scalar_only A flag specifying whether to only count parameters with one term.
-#' @param na.rm A flag specifying whether to remove invalid or missing term elements
+#' @param repair A flag specifying whether to repair the term vector.
+#' @param na.rm A flag specifying whether to remove missing term elements
 #' before getting the number of parameters.
 #' @param ... Unused.
 #' @return A count of the number of parameters.
 #' @seealso \code{\link{pars}()}, \code{\link{term-vector}} 
-#' and \code{\link{valid_term}()}
+#' and \code{\link{repair_terms}()}
 #' @export
 #' 
 #' @examples
@@ -22,11 +23,12 @@ npars <- function(x, ...) UseMethod("npars")
 
 #' @describeIn npars Number of parameters of term vector
 #' @export
-npars.term <- function(x, scalar_only = FALSE, na.rm = FALSE, ...) {
+npars.term <- function(x, scalar_only = FALSE, repair = TRUE, na.rm = FALSE, ...) {
   check_flag(scalar_only)
+  check_flag(repair)
   check_flag(na.rm)
   check_unused(...)
-  x <- repair_terms(x, scalars = scalar_only)
+  if(repair) x <- repair_terms(x, scalars = scalar_only)
   if(anyNA(x)) {
     if(isFALSE(na.rm)) return(NA_integer_)
     x <- x[is.na(x)]
