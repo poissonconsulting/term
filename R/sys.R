1be5145f8664c897e7fb2975b7b6a1758832d89a
joethorley <joe@poissonconsulting.ca>
2019-07-30 09:11:30 -0700

    allow . and _ in terms

--- R/sys.R
+++ R/sys.R
@@ -1,2 +1,2 @@
-.par_name_pattern <- "[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]\\w*"
+.par_name_pattern <- "[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz][ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._]*"
 .index_pattern <- "[123456789][0123456789]*"
