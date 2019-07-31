437491d26c39e50d92de4a08b8602b2ba6607473
joethorley <joe@poissonconsulting.ca>
2019-07-31 16:35:36 -0700

    sorting assumes repaired

--- R/maths.R
+++ R/maths.R
@@ -1,8 +1,5 @@
 #' @export
 `>.term` <- function(e1, e2) {
-  e1 <- repair_terms(e1)
-  e2 <- repair_terms(e2)
-  
   if (identical(length(e1), 1L)) e1 <- rep(e1, length(e2))
   if (identical(length(e2), 1L)) e2 <- rep(e2, length(e1))
   
