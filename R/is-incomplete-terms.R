0b2b8aae004697ea6581b0a79184517bcb257870
joethorley <joe@poissonconsulting.ca>
2019-07-31 17:03:20 -0700

    is.incomplete no missing values

--- R/is-incomplete-terms.R
+++ R/is-incomplete-terms.R
@@ -1,33 +1,24 @@
 #' Is Incomplete Terms
 #'
 #' Tests whether a term vector has absent elements.
-#' 
-#' The test is performed after the object's terms have been repaired.
-#' By default if the term has any missing values then NA is return.
 #'
 #' @param x An object.
-#' @param na.rm A flag specifying whether to ignore missing values.
 #' @param ... Unused
 #' @return A logical scalar indicating whether the object's terms are incomplete.
-#' @seealso \code{\link{term-vector}()}, \code{\link{repair_terms}()} 
-#' and \code{\link{complete_terms}()}
+#' @seealso \code{\link{term-vector}()} and \code{\link{complete_terms}()}
 #' @export
 #' 
 #' @examples
 #' is.incomplete_terms(as.term("b[2]"))
 #' is.incomplete_terms(as.term(c("b[2]", "b[1]")))
-#' is.incomplete_terms(as.term(c("b[2]", "b[1]", NA)))
 is.incomplete_terms <- function(x, ...) UseMethod("is.incomplete_terms")
 
 #' @describeIn is.incomplete_terms Test whether a term vector is incomplete
 #' @export
-is.incomplete_terms.term <- function(x, na.rm = FALSE, ...) {
-  if(anyNA(x)) {
-    if(isFALSE(na.rm)) return(NA)
-    x <- x[!is.na(x)]
-  }
+is.incomplete_terms.term <- function(x, ...) {
+  chk_no_missing(x)
+
   if(!length(x)) return(FALSE)
-  x <- repair_terms(x)
   x <- unique(x)
   length(x) < sum(vapply(pdims(x), prod, 1))
 }
--- tests/testthat/test-is-incomplete-terms.R
+++ tests/testthat/test-is-incomplete-terms.R
@@ -3,11 +3,10 @@ context("is-incomplete-terms")
 test_that("is.incomplete_terms",{
   expect_false(is.incomplete_terms(term(0L)))
   expect_false(is.incomplete_terms(as.term("b")))
-  expect_identical(is.incomplete_terms(as.term(c("b", NA))), NA)
-  expect_false(is.incomplete_terms(as.term(c("b", NA)), na.rm = TRUE))
+  expect_error(is.incomplete_terms(as.term(c("b", NA))), "x must not have missing values")
   expect_false(is.incomplete_terms(as.term(c("b", "b[2]"))))
   expect_error(is.incomplete_terms(as.term(c("b", "b[2]", "b[4,] "))), 
-               "x must not have missing values")
+               "missing value where TRUE/FALSE needed")
   expect_false(is.incomplete_terms(as.term("b[1]")))
   expect_true(is.incomplete_terms(as.term("b[2]")))
   expect_false(is.incomplete_terms(as.term(c("b[2]", "b"))))
