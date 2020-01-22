fd20c863b064d34942e36887e9662d2e9e5f888a
joethorley <joe@poissonconsulting.ca>
2020-01-21 19:31:34 -0800

    nterms to universals

--- R/nterms.R
+++ R/nterms.R
@@ -1,24 +1,20 @@
-#' Number of Terms
+#' @importFrom universals nterms
+#' @export
+universals::nterms
+
+#' Number of Terms of a term
+#' 
+#' Gets the number of unique terms.
+#' 
+#' If the term vector includes
+#' missing values then it returns NA_integer.
 #'
-#' Gets the number of unique term elements of an object.
-#'
-#' By default if the vector includes
-#' missing values then it returns NA_integer_.
-#'
-#' @inheritParams params
-#' @return A count of the number of terms.
-#' @seealso [term-vector()]
+#' @inherit universals::nterms
 #' @export
 #'
 #' @examples
 #' nterms(as.term(c("alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]")))
 #' nterms(as.term(c("alpha[1]", "alpha[1]", "beta[1,1]", "beta[1,1]")))
-nterms <- function(x, ...) {
-  UseMethod("nterms")
-}
-
-#' @describeIn nterms Number of unique term elements of term vector
-#' @export
 nterms.term <- function(x, ...) {
   if (anyNA(x)) {
     return(NA_integer_)
