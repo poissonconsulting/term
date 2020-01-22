ae0258abf80c05ebf75dd273175baf9e6a8a21ec
joethorley <joe@poissonconsulting.ca>
2020-01-21 18:27:30 -0800

    import npars from universals

--- R/npars.R
+++ R/npars.R
@@ -1,22 +1,17 @@
-#' Number of Parameters
-#'
-#' Gets the number of parameters of an object as returned by [pars()].
-#'
-#' @inheritParams params
+#' @importFrom universals npars
+#' @export
+universals::npars
+
+#' Number of Parameters of a term
+#' 
+#' @inherit universals::dims
 #' @param scalar A flag specifying whether to only count parameters with one term.
-#' @return A count of the number of parameters.
-#' @seealso [pars()] and [term-vector()]
 #' @export
 #'
 #' @examples
 #' npars(as.term(c("sigma", "alpha[1]", "alpha[2]", "beta[1,1]", "beta[2,1]")))
 #' npars(as.term(c("sigma", "alpha[1]", "alpha[2]")), scalar = TRUE)
-npars <- function(x, ...) UseMethod("npars")
-
-#' @describeIn npars Number of parameters of term vector
-#' @export
 npars.term <- function(x, scalar = NA, ...) {
-  chk_lgl(scalar)
   chk_unused(...)
   if (anyNA(x)) {
     return(NA_integer_)
