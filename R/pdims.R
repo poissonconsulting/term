78c039ca403151bfd80231e7cf370b69594ee5bc
joethorley <joe@poissonconsulting.ca>
2020-01-21 19:34:36 -0800

    pdims to universals

--- R/pdims.R
+++ R/pdims.R
@@ -1,23 +1,21 @@
+#' @importFrom universals pdims
+#' @export
+universals::pdims
+
 #' Parameter Dimensions
 #'
-#' Gets the parameter dimensions of an object as a named list of the dimensions
-#' of each parameter.
+#' Gets the parameter dimensions of a term.
 #'
 #' Errors if the parameter dimensions are inconsistent.
 #'
 #' A named list of the dimensions of each parameter can be converted
 #' into the equivalent [term-vector()] using [term()].
 #'
-#' @inheritParams params
-#' @return A named list of the dimensions of each parameter.
-#' @seealso [dims()] and [term()]
+#' @inherit universals::pdims
 #' @export
 #'
 #' @examples
 #' pdims(as.term(c("alpha[1]", "alpha[3]", "beta[1,1]", "beta[2,1]")))
-pdims <- function(x, ...) UseMethod("pdims")
-
-#' @export
 pdims.term <- function(x, ...) {
   chk_not_any_na(x)
   if (is.inconsistent_terms(x)) {
