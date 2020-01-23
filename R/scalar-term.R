2932548c336d7dfa3cc5c35a5fc827c819a9e9fe
joethorley <joe@poissonconsulting.ca>
2020-01-23 08:22:11 -0800

    scalar_term no longer repair

--- R/scalar-term.R
+++ R/scalar-term.R
@@ -2,8 +2,9 @@
 #'
 #' Test whether each element in a term vector is scalar.
 #' 
-#' The function tests for the absence of a \code{[} 
-#' after by default repairing the term vector.
+#' The function tests for the absence of a \code{[}.
+#' 
+#' A modified term vector may need to be repaired first.
 #'
 #'
 #' @inheritParams params
@@ -13,13 +14,11 @@
 #'
 #' @examples
 #' scalar_term(as.term(c("a", "a[2]", "b[1]", "c[1,1]")))
-scalar_term <- function(x, repair = TRUE) {
+#' scalar_term(repair_terms(as.term(c("a", "a[2]", "b[1]", "c[1,1]"))))
+scalar_term <- function(x) {
   chk_s3_class(x, "term")
   if (!length(x)) {
     return(logical(0))
-  }
-  if(vld_true(repair)) {
-    x <- repair_terms(x)
   }
   scalar <- !grepl("\\[", x)
   is.na(scalar[is.na(x)]) <- TRUE
