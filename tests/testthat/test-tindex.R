bb2c149a9d61984d5d9fe4032c7c5d990ba0d0fd
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-03 02:49:55 +0100

    Prefer set_names() over structure()

--- tests/testthat/test-tindex.R
+++ tests/testthat/test-tindex.R
@@ -17,21 +17,24 @@ test_that("tindex", {
 
   expect_identical(
     tindex(as.term(NA_character_)),
-    structure(list(NA_integer_), .Names = NA_character_)
+    rlang::set_names(list(NA_integer_), NA_character_)
   )
 
   expect_identical(
     tindex(as.term(c("a", NA_character_))),
-    structure(list(1L, NA_integer_), .Names = c("a", NA))
+    rlang::set_names(list(1L, NA_integer_), c("a", NA))
   )
 
-  expect_identical(tindex(NA_term_), structure(list(NA_integer_), .Names = NA_character_))
+  expect_identical(
+    tindex(NA_term_),
+    rlang::set_names(list(NA_integer_), NA_character_)
+  )
 
   expect_identical(
     tindex(as.term(c("alpha", "alpha[2]", "beta[1,1]", "beta[2 ,1  ]", NA))),
-    structure(list(1L, 2L, c(1L, 1L), 2:1, NA_integer_), .Names = c(
-      "alpha",
-      "alpha[2]", "beta[1,1]", "beta[2 ,1  ]", NA
-    ))
+    rlang::set_names(
+      list(1L, 2L, c(1L, 1L), 2:1, NA_integer_),
+      c("alpha", "alpha[2]", "beta[1,1]", "beta[2 ,1  ]", NA)
+    )
   )
 })
