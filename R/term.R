12a46736781784dec6810f4a9d94469dceec0818
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-03 19:04:46 +0100

    Reimagine term()

--- R/term.R
+++ R/term.R
@@ -1,9 +1,14 @@
 #' Term Vector
 #'
-#' Creates a term vector from object dimensions.
-#' A `term` vector is a S3 vector of parameter terms.
+#' Creates a term vector from values.
+#' A `term` vector is an S3 vector of parameter terms
+#' of the form `p`, `q[#]` or `r[#,#]` where `#` are
+#' positive integers.
+#' This function checks that all terms are valid
+#' but does not require stronger levels of consistency,
+#' see `chk_valid()` for details.
 #'
-#' @param x An integer vector of the object dimensions or a named list of the parameter's dimensions.
+#' @param ... Term values.
 #' @inheritParams params
 #'
 #' @return A term vector.
@@ -12,58 +17,75 @@
 #' @export
 #'
 #' @examples
-#' term(0L)
-#' term(1L)
-#' term(2L)
-term <- function(x, ...) UseMethod("term")
+#' term()
+#' term("p", "q[1]", "q[2]", "q[3]")
+#' term("q[1]", "q[2]", "q[3]")
+#' combimed <- term(par = 2:4, "alpha")
+#' pdims(combined)
+#' term(!!!pdims(combined))
+#' try(term("r["))
+term <- function(...) {
+  args <- list2(...)
+  compat_args <- exec(term_compat_args, !!!args)
+  if (is.numeric(compat_args$x)) {
+    lifecycle::deprecate_soft(
+      "0.2.0", "term::term(x =)",
+      details = "Use named arguments to pass integer dimensions."
+    )
+    return(term_impl(list2(!!compat_args$name := compat_args$x)))
+  } else if (is.list(compat_args$x)) {
+    lifecycle::deprecate_soft(
+      "0.2.0", "term::term(x =)",
+      details = "Use named arguments directly, or splice a list as in `term(!!!x)`."
+    )
+    return(term_impl(compat_args$x))
+  }
 
-#' @export
-term.NULL <- function(x, name = "par", ...) {
-  chk_unused(...)
-  term(0L, name = name)
+  term_impl(args)
 }
 
-#' @describeIn term Term vector from an integer vector of the object's dimensions
-#' @export
-term.integer <- function(x, name = "par", ...) {
-  chk_string(name)
-  chk_unused(...)
+term_impl <- function(args) {
+  named <- (names2(args) != "")
+  unnamed_args <- unname(args[!named])
+  chk_all(unnamed_args, chk_string)
+  chk_term(new_term(unlist_chr(unnamed_args)), "valid")
 
-  if (!length(x)) {
-    return(as.term(character(0)))
-  }
-  if (any(x == 0L)) {
-    return(as.term(character(0)))
-  }
-  if (identical(x, 1L)) {
-    return(as.term(name))
-  }
-  if (identical(length(x), 1L)) {
-    return(as.term(p0(name, "[", 1:x, "]")))
-  }
+  named_args <- args[named]
+  chk_all(named_args, chk_whole_numeric)
+  chk_all(named_args, chk_gte)
 
-  x <- lapply(x, function(x) 1:x)
+  args[named] <- mapply(
+    term_from_pdims,
+    named_args, names(named_args),
+    SIMPLIFY = FALSE
+  )
+
+  new_term(unlist_chr(unname(args)))
+}
+
+term_compat_args <- function(`_x` = x, name = "par", ..., x = NULL) {
+  list(x = `_x`, name = name)
+}
+
+term_from_pdims <- function(x, name) {
+  if (is_empty(x)) {
+    # FIXME: Return new_term(name) instead?
+    return(new_term())
+  }
+  if (any(x == 0)) {
+    return(new_term())
+  }
+  if (length(x) == 1 && x == 1) {
+    return(new_term(name))
+  }
+  x <- lapply(x, seq_len)
   x <- do.call("expand.grid", x)
   x <- as.matrix(x)
   x <- apply(x, 1L, function(x) p(x, collapse = ","))
   x <- p0(name, "[", x, "]")
-  as.term(x)
+  new_term(x)
 }
 
-#' @export
-term.numeric <- function(x, name = "par", ...) {
-  chk_unused(...)
-  term(as.integer(x), name = name)
-}
-
-#' @export
-term.list <- function(x, ...) {
-  if (!length(x)) {
-    return(term(0L))
-  }
-  chk_named(x)
-  x <- mapply(term, x, names(x))
-  x <- unlist(x)
-  x <- as.vector(x)
-  as.term(x)
+unlist_chr <- function(x) {
+  vec_c(!!!x, .ptype = character())
 }
