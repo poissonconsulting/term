6e039532b5437bf9670c3603fb51c5ed1a0a433c
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-03 19:33:22 +0100

    Indent

--- tests/testthat/test-vld.R
+++ tests/testthat/test-vld.R
@@ -6,11 +6,11 @@ test_that("vld_term", {
   expect_true(vld_term(new_term(character(0))))
   expect_true(vld_term(new_term(NA_character_)))
 
-    rlang::with_options(lifecycle_verbosity = "error", {
-      expect_error(vld_term(new_term(c("x[2]", "x[1")), validate = "class"), class = "defunctError")
-    })
+  rlang::with_options(lifecycle_verbosity = "error", {
+    expect_error(vld_term(new_term(c("x[2]", "x[1")), validate = "class"), class = "defunctError")
+  })
   rlang::with_options(lifecycle_verbosity = "quiet", {
-  expect_true(vld_term(new_term(c("x[2]", "x[1")), validate = "class"))
+    expect_true(vld_term(new_term(c("x[2]", "x[1")), validate = "class"))
   })
   expect_false(vld_term(new_term(c("x[2]", "x[1")), validate = "valid"))
   expect_true(vld_term(new_term(c("x[2]", "x[1]")), validate = "valid"))
