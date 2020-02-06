87b0e9a92b61ab8290bc4a9c34b5933a0981986f
joethorley <joe@poissonconsulting.ca>
2020-02-05 19:17:09 -0800

    npars_scalar to term

--- R/npars-scalar.R
+++ R/npars-scalar.R
@@ -1,14 +1,12 @@
 #' @export
 universals::npars_scalar
 
-#' Number of Dimensions of each Parameter
-#'
 #' @inherit universals::npars_scalar
 #' @export
 #'
 #' @examples
 #' npars_scalar(term("alpha[1]", "alpha[3]", "beta[1,1]", "beta[2,1]"))
-npars_scalar.default <- function(x, ...) {
+npars_scalar.term <- function(x, ...) {
   if (anyNA(x)) {
     return(NA_integer_)
   }
