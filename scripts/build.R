c0129e21eb12c1cf22d117cda424feed146e8fdd
Joe Thorley <joe@poissonconsulting.ca>
2020-07-10 13:14:57 -0700

    update build script
    skip ci

--- scripts/build.R
+++ scripts/build.R
@@ -1,12 +1,11 @@
 roxygen2md::roxygen2md()
-
 styler::style_pkg(filetype = c("R", "Rmd"))
+lintr::lint_package()
 
 devtools::test()
 devtools::document()
-# knitr::knit("README.Rmd")
-if(FALSE) {
-  if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
-  pkgdown::build_site()
-}
+
+rmarkdown::render("README.Rmd", output_format = "md_document")
+pkgdown::build_site()
+
 devtools::check()
