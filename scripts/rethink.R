3ec77a2c6793bcc7e57815f8ddcf0f039371a15b
Joe Thorley <joe@poissonconsulting.ca>
2020-07-10 18:32:51 -0700

    rethink text

--- scripts/rethink.R
+++ scripts/rethink.R
@@ -11,18 +11,29 @@ nlist
 as_term(nlist)
 
 # by themselves such term vectors are not very interesting
+# however since they are use in a range of structures to identify elements they
 
-# however when used to identify elements in various structures they start to become interesting
+mcmc <- as_mcmc(nlist)
+mcmc
 
-mcmc <- as.mcmc(nlist)
-term_frame <- as_term_frame(nlist)
-
-# as they allow reconstruction of the original numeric objects
-
-as_nlist(term_frame)
-
+# are interesting because they can be used to start to manipulate the elements
+#
+as_nlist(mcmc)
 
 
+# its important to note that elements can be missing, duplicated, inconsistent
 
+mcmc <- mcmc[,-c(2,7),drop = FALSE]
+
+mcmc
+
+as_nlist(mcmc)
+
+# the term packages is designed to manipulate term vectors.
+#
+# currently it is based on a character vector as a vctrs_vctr which has to be parsed into its component parts for each term ie the name of the parameter and the index.
+#
+# the question is whether its worth defining each element in a term vector as a composite of scalars of other class (possibly vctr_vctrs) one class is the parameter
+# name and the other is the index which must be a 1 or more positive integers?
 
 
