83930fdafe12a5e591ac664dd5dd97fb2623d119
Joe Thorley <joe@poissonconsulting.ca>
2020-07-13 10:33:42 -0700

    test new_term_rcrd()

--- R/term-rcrd.R
+++ R/term-rcrd.R
@@ -1,8 +1,19 @@
+#' Construct a new term_rcrd object
+#'
+#' Use this function to quickly construct a term_rcrd object.
+#'
+#' @param x A data frame with columns par and dim.
+#'
 #' @export
-new_term_rcrd <- function(x = data.frame(par = character(), dim = list(), stringsAsFactors = FALSE)) {
-  stopifnot(is.data.frame(x))
-  stopifnot(grepl("^par$|^dim$", names(x)))
-  #stopifnot(grepl("^par$|^dim[1-9][0-9]*$", names(x)))
+#' @examples
+#' new_term_rcrd()
+#' new_term_rcrd(data.frame(par = c("x", "x", "y"), dim = I(list(1, 2, c(2,2))),
+#' stringsAsFactors = FALSE))
+#' @export
+new_term_rcrd <- function(x = data.frame(par = character(), dim = I(list()), stringsAsFactors = FALSE)) {
+  chk_data(x)
+  check_names(x, c("par", "dim"))
+  x$dim <- as_list(x$dim)
   vctrs::new_data_frame(x, class = "term_rcrd")
 }
 
---
+++ tests/testthat/test-term-rcrd.R
@@ -0,0 +1,10 @@
+test_that("new_term_rcrd", {
+  expect_identical(new_term_rcrd(),
+                   structure(list(par = character(0), dim = list()), class = c("term_rcrd",
+                                                                               "data.frame"), row.names = integer(0)))
+  expect_identical(new_term_rcrd(data.frame(par = c("x", "x", "y"), dim = I(list(1, 2, c(2,2))),
+                           stringsAsFactors = FALSE)),
+  structure(list(par = c("x", "x", "y"), dim = list(1, 2, c(2,
+                                                            2))), class = c("term_rcrd", "data.frame"), row.names = c(NA,
+                                                                                                                      -3L)))
+})
