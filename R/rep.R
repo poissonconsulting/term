c3994af244efb983e6f9d395e7ae9e75ca2a6d2f
Joe Thorley <joe@poissonconsulting.ca>
2020-07-14 12:08:02 -0700

    - Added rep.term_rcrd().

--- R/rep.R
+++ R/rep.R
@@ -1,4 +1,12 @@
 #' @export
 rep.term <- function(x, times, ...) {
+  chk_unused(...)
   new_term(rep(as.character(x), times))
+}
+
+#' @export
+rep.term_rcrd <- function(x, times, ...) {
+  chk_unused(...)
+  x <- data.frame(par = rep(x$par, times), dim = I(rep(x$dim, times)))
+  new_term_rcrd(x)
 }
--- tests/testthat/test-rep.R
+++ tests/testthat/test-rep.R
@@ -1,5 +1,11 @@
-test_that("rep", {
+test_that("rep term", {
   expect_identical(rep(as_term("a")), as_term("a"))
   expect_identical(rep(as_term("a"), 2), as_term(c("a", "a")))
   expect_identical(rep(as_term(c("a[2]", "a[1]")), 2), as_term(rep(c("a[2]", "a[1]"), 2)))
+})
+
+test_that("rep term_rcrd", {
+  expect_identical(rep(as_term_rcrd("a")), as_term_rcrd("a"))
+  expect_identical(rep(as_term_rcrd("a"), 2), as_term_rcrd(c("a", "a")))
+  expect_identical(rep(as_term_rcrd(c("a[2]", "a[1]")), 2), as_term_rcrd(rep(c("a[2]", "a[1]"), 2)))
 })
