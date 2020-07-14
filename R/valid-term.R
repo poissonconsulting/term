bef615e52c846231e331d74fe152dec1d802a2a3
Joe Thorley <joe@poissonconsulting.ca>
2020-07-14 12:25:11 -0700

    working on valid

--- R/valid-term.R
+++ R/valid-term.R
@@ -1,6 +1,6 @@
 #' Valid Terms
 #'
-#' Test whether each element in a term vector is valid.
+#' Test whether each element in a term or term_rcrd object is valid.
 #'
 #' Repairing a term vector replaces invalid terms with a missing value.
 #'
@@ -15,6 +15,14 @@
 #' # invalid term elements
 #' valid_term(new_term(c("a b", "a[1]b", "a[0]", "b[1,]", "c[]", "d[1][2]")))
 valid_term <- function(x) {
-  chk_s3_class(x, "term")
-  valid_term_impl(x)
+  chkor(chk_s3_class(x, "term"), chk_s3_class(x, "term_rcrd"))
+  if(is_term(x)) return (valid_term_impl(x))
+  valid <- grepl(pattern , x)
+
+  pattern <- p0("^\\s*", .par_name_pattern, "\\s*$")
+  valid_par <- grepl(pattern, x$par)
+  valid_par[is.na(x$par)] <- NA
+  valid_dim <-vapply(x$dim, is_index, TRUE)
+    valid_par[vapply(x$par, anyNA, TRUE)] <- NA
+  valid_par & valid_dim
 }
--- R/vld.R
+++ R/vld.R
@@ -59,3 +59,59 @@ vld_term <- function(x, validate = "complete") {
   }
   !is_incomplete_terms(x)
 }
+
+#' Validate Term Record
+#'
+#' Validates the elements of a term_rcrd object.
+#' Use [chk_s3_class()] to check if an object is a term_rcrd.
+#'
+#' Internal validity of a term can be checked on three levels:
+#'
+#' - `"valid"` checks that all terms are of the form
+#'   `x`, `x[#]`, `x[#,#]` etc. where `x` is an identifier
+#'   and `#` are positive integers.
+#' - `"consistent"` checks that all terms are addressed
+#'   with the same dimensionality; the terms `x[1]` and `x[2,3]`
+#'   are inconsistent.
+#' - `"complete"` checks that the values span all possible values
+#'   across all dimensions; if `x[3,4]` exist, the vector must
+#'   contain at least 11 more terms to be consistent
+#'   (`x[1,1]` to `x[1,4]`, `x[2,1]` to `x[2,4]` and
+#'   `x[3,1]` to `x[3,3]`).
+#'
+#' Missing values are ignored as are duplicates and order.
+#'
+#' @inheritParams params
+#' @inheritParams chk::chk_flag
+#' @return A flag indicating whether the condition was met.
+#' @seealso [chk_term_rcrd()]
+#' @export
+#' @examples
+#' vld_term_rcrd(c("x[2]", "x[1]"))
+#' vld_term_rcrd(term_rcrd("x[2]", "x[1]"))
+vld_term_rcrd <- function(x, validate = "complete") {
+  chk_string(validate)
+  chk_subset(validate, c("valid", "consistent", "complete"))
+
+  if (!is_term_rcrd(x)) {
+    return(FALSE)
+  }
+  x <- x[!is.na(x$par) & !is.na(x$dim), , drop = FALSE]
+  if (!nrow(x)) {
+    return(TRUE)
+  }
+
+  if (!all(valid_term(x))) {
+    return(FALSE)
+  }
+  if (validate == "valid") {
+    return(TRUE)
+  }
+  if (is_inconsistent_terms(x)) {
+    return(FALSE)
+  }
+  if (validate == "consistent") {
+    return(TRUE)
+  }
+  !is_incomplete_terms(x)
+}
