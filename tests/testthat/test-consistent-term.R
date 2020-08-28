02086f0e2cc968073f411bcd4c7ded4883725865
Joe Thorley <joe@poissonconsulting.ca>
2020-08-28 12:51:54 -0700

    more consistent term tests

--- tests/testthat/test-consistent-term.R
+++ tests/testthat/test-consistent-term.R
@@ -20,8 +20,13 @@ test_that("consistent_term term_rcrd", {
   expect_error(consistent_term(1), "`x` must inherit from S3 class 'term_rcrd'.",
                class = "chk_error"
   )
-  expect_identical(consistent_term(as_term_rcrd(new_term(c("a[1,1]", "a[2,1]", "a[", "b")))), c(FALSE, FALSE, FALSE, TRUE))
+  expect_identical(consistent_term(term_rcrd()), logical(0))
+  expect_identical(consistent_term(NA_term_), NA)
+  expect_identical(consistent_term(term_rcrd("a")), TRUE)
+  expect_identical(consistent_term(term_rcrd(c("a", "a"))), c(TRUE, TRUE))
+  expect_identical(consistent_term(term_rcrd(c("a", "a[1]"))), c(TRUE, TRUE))
+  expect_identical(consistent_term(term_rcrd(c("a", "a[2]"))), c(TRUE, TRUE))
+  expect_identical(consistent_term(term_rcrd(c("a[10]", "a[2]"))), c(TRUE, TRUE))
+  expect_identical(consistent_term(term_rcrd(c("a[2]", "a[2,1]"))), c(FALSE, FALSE))
+  expect_identical(consistent_term(term_rcrd(c("a[1,1]", "a[2,1]"))), c(TRUE, TRUE))
 })
-
-
-
