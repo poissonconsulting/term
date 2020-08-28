73f9bcd170550741d1035708a30d65991ad87cb0
Joe Thorley <joe@poissonconsulting.ca>
2020-08-28 11:12:17 -0700

    - Added dims.term_rcrd()
    closes #45

--- R/dims.R
+++ R/dims.R
@@ -10,3 +10,13 @@ universals::dims
 dims.term <- function(x, ...) {
   length(x)
 }
+
+#' @inherit universals::dims
+#' @export
+#' @examples
+#'
+#' dims(term_rcrd("beta[1,1]"))
+#' dims(term_rcrd("beta[1,1]", "beta[1,2]"))
+dims.term_rcrd <- function(x, ...) {
+  length(x)
+}
--- tests/testthat/test-dims.R
+++ tests/testthat/test-dims.R
@@ -3,3 +3,10 @@ test_that("dims.term", {
   expect_identical(dims(new_term("beta0")), 1L)
   expect_identical(dims(new_term(c("beta[1,1]", "beta[2,1]"))), 2L)
 })
+
+test_that("dims.term_rcrd", {
+  expect_identical(dims(new_term_rcrd()), 0L)
+  expect_identical(dims(term_rcrd("beta0")), 1L)
+  expect_identical(dims(term_rcrd(c("beta[1,1]", "beta[2,1]"))), 2L)
+})
+
