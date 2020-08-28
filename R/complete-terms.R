18fabc8341fc0d8858bff143ce842eff7acd2f7b
Joe Thorley <joe@poissonconsulting.ca>
2020-08-28 12:43:53 -0700

    - Added complete_terms.term_rcrd().

--- R/complete-terms.R
+++ R/complete-terms.R
@@ -2,7 +2,10 @@
 #'
 #' Completes an object's terms.
 #'
+#' @details It must not have any invalid or missing (NA) values.
+#'
 #' @inheritParams params
+#' @seealso [term-vector()], [repair_terms()] and [is_incomplete_terms()].
 #' @export
 complete_terms <- function(x, ...) {
   UseMethod("complete_terms")
@@ -10,16 +13,14 @@ complete_terms <- function(x, ...) {
 
 #' @describeIn complete_terms Complete Terms for a term Vector
 #'
-#' @details It must not have any invalid or missing (NA) values.
-#'
-#' @seealso [term-vector()], [repair_terms()] and [is_incomplete_terms()].
 #' @export
 #'
 #' @examples
 #' complete_terms(term("b[3]", "b[1]", "b[2]"))
 #' complete_terms(term("z[2,2]", "z[1,1]"))
 complete_terms.term <- function(x, ...) {
-  if (!length(x)) {
+  chk_unused(...)
+  if (!dims(x)) {
     return(x)
   }
   x <- repair_terms(x)
@@ -27,4 +28,20 @@ complete_terms.term <- function(x, ...) {
     abort_chk("`x` must not have any missing values.")
   }
   term(x, !!!setdiff(term_impl(pdims(x)), x))
+}
+
+
+#' @describeIn complete_terms Complete Terms for a term_rcrd vector
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' complete_terms(term_rcrd("b[3]", "b[1]", "b[2]"))
+#' complete_terms(term_rcrd("z[2,2]", "z[1,1]"))
+#' }
+complete_terms.term_rcrd <- function(x, ...) {
+  if(!dims(x)) return(x)
+  as_term_rcrd(complete_terms(as_term(x), ...))
+
 }
--- tests/testthat/test-complete-terms.R
+++ tests/testthat/test-complete-terms.R
@@ -1,4 +1,4 @@
-test_that("complete_terms", {
+test_that("complete_terms term", {
   expect_error(complete_terms(NA_term_), "^`x` must not have any missing values[.]$",
     class = "chk_error"
   )
@@ -23,5 +23,30 @@ test_that("complete_terms", {
   )
   expect_error(complete_terms(new_term(c("b", "b[2,2]"))),
                    "`x` must have terms with consistent parameter dimensions.",
+               class = "chk_error")
+})
+
+test_that("complete_terms term_rcrd", {
+  expect_identical(complete_terms(new_term_rcrd()), new_term_rcrd())
+  expect_error(
+    complete_terms(term_rcrd(c(NA_term_, "b[2]"))),
+    "^`x` must not have any missing values[.]$",
+    class = "chk_error"
+  )
+  expect_identical(complete_terms(term_rcrd("b")), term_rcrd("b"))
+  expect_identical(complete_terms(term_rcrd(c("b", "b"))), term_rcrd(c("b", "b")))
+  expect_identical(complete_terms(term_rcrd("b")), term_rcrd("b"))
+  expect_identical(complete_terms(term_rcrd("b[1]")), term_rcrd("b"))
+  expect_identical(
+    complete_terms(term_rcrd(c("b", "b[3]"))),
+    term_rcrd(c("b[1]", "b[3]", "b[2]"))
+  )
+  expect_identical(complete_terms(term_rcrd("b[3]")), term_rcrd(c("b[3]", "b[1]", "b[2]")))
+  expect_identical(
+    complete_terms(term_rcrd(c("z[2,2]", "z[2,1]"))),
+    term_rcrd(c("z[2,2]", "z[2,1]", "z[1,1]", "z[1,2]"))
+  )
+  expect_error(complete_terms(term_rcrd(c("b", "b[2,2]"))),
+               "`x` must have terms with consistent parameter dimensions.",
                class = "chk_error")
 })
