a927064c5921fa3801ac76b70917904b832d1d9d
Evan <evanamiesgalonski@gmail.com>
2022-02-28 16:43:56 -0800

    deprecated default.stringsAsFactors to FALSE

--- R/as-data-frame.R
+++ R/as-data-frame.R
@@ -1,6 +1,6 @@
 #' @method as.data.frame term_rcrd
 #' @export
-as.data.frame.term_rcrd <- function(x, ..., stringsAsFactors = default.stringsAsFactors()) {
+as.data.frame.term_rcrd <- function(x, ..., stringsAsFactors = FALSE) {
   chk_flag(stringsAsFactors)
   # FIXME: Need stringsAsFactors? Use vctrs::new_data_frame()?
   data.frame(par = field(x, "par"), dim = I(field(x, "dim")), stringsAsFactors = stringsAsFactors)
--- tests/testthat/test-as-data-frame.R
+++ tests/testthat/test-as-data-frame.R
@@ -1,7 +1,7 @@
 test_that("as.data.frame.term_rcrd", {
   expect_identical(as.data.frame(term_rcrd()),
-                   data.frame(par = character(), dim = I(list()), stringsAsFactors = default.stringsAsFactors()))
+                   data.frame(par = character(), dim = I(list()), stringsAsFactors = FALSE))
   expect_identical(as.data.frame(term_rcrd("a")),
-                   data.frame(par = "a", dim = I(list(1L)), stringsAsFactors = default.stringsAsFactors()))
+                   data.frame(par = "a", dim = I(list(1L)), stringsAsFactors = FALSE))
 
 })
