4ba32bc861e0a8d7b095ee64a27fceb3e7306ab5
Kirill Müller <kirill@cynkra.com>
2026-01-27 09:34:33 +0100

    Thanks Copilot

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -4,13 +4,13 @@ test_that("summary.term", {
     "Dummy"
   })
 
+  expect_identical(summary(term())[1:3], summary(character(0))[1:3])
+  expect_identical(summary(term("a[1]"))[1:3], summary("a[1]")[1:3])
+
   skip_if_not(getRversion() >= "4.6")
 
   expect_snapshot({
     summary(term("a[1]"))
     summary("a[1]")
   })
-
-  expect_identical(summary(term())[1:3], summary(character(0))[1:3])
-  expect_identical(summary(term("a[1]"))[1:3], summary("a[1]")[1:3])
 })
